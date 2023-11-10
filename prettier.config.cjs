8908b994d9c8c5d288c8741661fe9ad78312c67f
exsjabe <jakob.bergstrom@explizit.se>
2023-11-10 15:58:44 +0100

    prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
+
+module.exports = config;
